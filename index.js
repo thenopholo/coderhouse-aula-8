802c5225b1eb4f7184db68d7a8e1936357cf39a5
Rodrigo Thenopholo <thenopholo92@gmail.com>
2024-02-22 16:04:27 -0300

    Ramovida um comentario

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ document.querySelector('.form').addEventListener('submit', function (event) {
     let nome = document.getElementById("nomeInput").value;
     let sobrenome = document.getElementById("sobrenomeInput").value;
     let email = document.getElementById("emailInput").value;
-    let telefone = document.getElementById("telefoneInput").value; // Adicionado campo de telefone
+    let telefone = document.getElementById("telefoneInput").value;
     let senha = document.getElementById("senhaInput").value;
 
     let usuario = new novoUsuario(nome, sobrenome, email, telefone, senha);
