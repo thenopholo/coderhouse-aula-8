1f7d28d2cb1b5978bbc53cf34003b33496475ef1
Raul Lima <raultorres.lima@gmail.com>
2024-03-14 20:39:16 -0300

    Implementa a atividade complementar da aula 12

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import { Usuario } from "./model/Usuario.js";
 
 
+
 document.querySelector('.form').addEventListener('submit', function (event) {
 
     let nome = document.getElementById("nomeInput").value;
@@ -13,4 +14,14 @@ document.querySelector('.form').addEventListener('submit', function (event) {
 
     console.log(usuario); // Isso imprimirá o objeto usuário no console
     usuario.exibeDados(); // Isso imprimirá os dados do usuário em um alerta
+
+
+    // Atividade complementar aula 12
+    console.log(usuario || "Erro: usuário não foi criado");
+    let operacao = prompt("Caso deseje redefinir a senha, digite SIM").toUpperCase();
+    console.log(operacao)
+    operacao == "SIM" ? usuario.senha = prompt("Digite a nova senha:") : alert("Cadastro finalizado!");
+    usuario.exibeDados();
+
 });
+
